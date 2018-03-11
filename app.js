401a3f69701b62c68709571119e08f6414fbd492
Lukas Karas <lukas.karas@iflix.com>
2018-03-11 19:47:58 +0000

    Server is not started during tests to prevent async tests starting server on the same port.

--- app.js
+++ app.js
@@ -7,14 +7,15 @@ app.use(bodyParser.urlencoded({ limit: '5mb', extended: false }))
 
 app.use((req, res, next) => {
   console.log('calling ' + req.method + ' for ' + req.path + ' -> ' + JSON.stringify(req.query) + ' -> ' + JSON.stringify(req.body))
-
   next()
 })
 
 app.use('/', require('./routes/index'))
 
-app.listen(3000, () => {
-  console.log('listening on port 3000')
-})
+if (process.env.NODE_ENV !== 'test') {
+  app.listen(3000, () => {
+    console.log('listening on port 3000')
+  })
+}
 
 module.exports = app
